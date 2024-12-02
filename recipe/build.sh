ca777e9dff519e88bbbc61f3d14f4ed0b274d15c
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-01 18:34:55 -0800

    enable rattler-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 # check licenses
 cargo-bundle-licenses \
     --format yaml \
@@ -16,6 +19,3 @@ mkdir -p ${PREFIX}/share/zsh/site-functions
 install -m 644 completions/bash/code-minimap.bash ${PREFIX}/etc/bash_completion.d/code-minimap.bash
 install -m 644 completions/fish/code-minimap.fish ${PREFIX}/share/fish/vendor_completions.d/code-minimap.fish
 install -m 644 completions/zsh/_code-minimap ${PREFIX}/share/zsh/site-functions/_code-minimap
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/${PKG_NAME}"
